2ae2f647abb2982548b1e1548be1c83f106dc1c0
minhtuyet2000 <minhtuyet1908002@gmail.com>
2025-08-31 17:47:02 +0700

    Update 31/08

--- src/main/java/utilities/ExtentManager.java
+++ src/main/java/utilities/ExtentManager.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class ExtentManager {
@@ -13,6 +14,11 @@ public class ExtentManager {
                 "./reports/ExtentReport.html";
         ExtentSparkReporter reporter = new
                 ExtentSparkReporter(reportPath);
+        try {
+            reporter.loadXMLConfig("src/main/resources/extent-config.xml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         reporter.config().setDocumentTitle("Automation Test Report");
         reporter.config().setTimeStampFormat("yyyy-MM-dd HH:mm:ss");
         Locale.setDefault(Locale.ENGLISH);
